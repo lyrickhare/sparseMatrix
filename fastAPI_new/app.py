1c6933d817b31478248cd4a6449e3e6813fc1bf1
lyrickhare <lyrickhare@gmail.com>
2024-02-10 19:11:19 +0530

    issue in -1 pin

--- fastAPI_new/app.py
+++ fastAPI_new/app.py
@@ -5,13 +5,15 @@ app = FastAPI()
 ob = utilClass.sparseMat()  
 
 @app.post("/insertMerchant")
-async def insMer(pinCode : int= Form(...), merchID : int = Form(...)):
+async def insMer(pinCode : int= Form(...), merchID : str = Form(...)):
+    ob.load_data()
     ob.insert_merchant(pinCode,merchID)
     ob.save_data()
     return {"result" : "successfully inserted"}
 
 @app.post("/getMerchants")
 async def getMer(pinCode : int= Form(...)):
+    ob.load_data()
     a = ob.get_merchants(pinCode)
     if(a!=0):
         return  {str(pinCode):a}
@@ -20,11 +22,13 @@ async def getMer(pinCode : int= Form(...)):
 
 @app.post("/delDB")
 async def delDB(): 
+    ob.load_data()
     ob.delete_json()
     return {"result" : "successfully deleted the database"}
 
 @app.post("/delMerchant")
-async def insMer(pinCode : int= Form(...), merchID : int = Form(...)):
+async def insMer(pinCode : int= Form(...), merchID : str = Form(...)):
+    ob.load_data()
     a = ob.delete_merchant(pinCode,merchID)
     if(a!=0):
         ob.save_data()
